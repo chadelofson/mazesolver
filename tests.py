b6839e02a55eb814ee78c7d2af29955bfc1911a8
chadelofson <chad.elofson@gmail.com>
2024-02-04 00:28:42 -0800

    Final base of project

---
+++ tests.py
@@ -0,0 +1,37 @@
+import unittest
+from maze import Maze
+
+class Tests(unittest.TestCase):
+    def test_maze_create_cells(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows
+        )
+
+    def test_maze_break_entrance_exit(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(m1._cells[0][0].has_top_wall, False)
+        self.assertEqual(m1._cells[num_cols-1][num_rows-1].has_bottom_wall, False)
+
+    def test_maze_visited_is_false(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        is_visited = False
+        for i in range(num_cols):
+            for j in range(num_rows):
+                if m1._cells[i][j]._visited is True:
+                    is_visited = True
+        self.assertEqual(is_visited, False)
+
+if __name__ == '__main__':
+    unittest.main()
